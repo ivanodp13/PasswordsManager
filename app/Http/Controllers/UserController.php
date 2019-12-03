5dd55729d7d814b26f79b2bc9a1ba90f1366cd30
Iván Obejo <ivanobejo@hotmail.es>
2019-12-03 11:30:02 +0100

    Login and Register created. Token class created. JWT installed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,124 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Firebase\JWT\JWT;
+use App\Helpers\Token;
 
-class UserController extends Controller
+class user_controller extends Controller
 {
-    //
+
+    public function login(Request $request)
+    {
+        $data = ['email' => $request->email];
+        $user = User::where($data)->first();
+
+        if($user->password == $request->password)
+        {
+
+            $token = new token($data);
+            $token = $token->encode();
+
+            return response()->json([
+                "token" => $token
+            ],200);
+        }
+
+        return response()->json([
+            "message" => 'unauthorized'
+        ],401);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        $token = new token(['email' => $user->email]);
+        $token = $token->encode();
+
+
+        return response()->json([
+            "token" => $token
+        ],200);
+
+        //$token = JWT::encode($data_token, $this->key);
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- app/User.php
+++ app/User.php
@@ -7,5 +7,31 @@ use Illuminate\Http\Request;
 
 class User extends Model
 {
+    protected $filliable = ['name','email','password'];
 
+    public function register(Request $request)
+    {
+        $user = new self();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return $user->email;
+    }
+
+    public function login(Request $request)
+    {
+        $user = new self();
+        $user_email = $request->email;
+        $user_password = $request->password;
+
+        $email = DB::table('users')->where('email', $user_email)->value('email');
+        $password = DB::table('users')->where('password', $user_password)->value('password');
+
+        var_dump($email);
+
+        return $email;
+
+    }
 }
